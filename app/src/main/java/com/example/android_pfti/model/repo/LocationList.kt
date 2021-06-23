99998e0f6514d02da17009f88cb0793428585668
Aleksandr <mr.nevniskiy@gmail.com>
2021-06-24 01:39:44 +0600

    -fix

--- app/src/main/java/com/example/android_pfti/model/repo/LocationList.kt
+++ app/src/main/java/com/example/android_pfti/model/repo/LocationList.kt
@@ -9,4 +9,7 @@ object LocationList {
         Pair(LatLng(54.0, 82.0), "Second"),
         Pair(LatLng(-34.0, 151.0), "Third")
     )
+    fun edit(){
+        arrayOfMarkers[0] = Pair(LatLng(93.0, 151.0), "First")
+    }
 }
--- app/src/main/java/com/example/android_pfti/view/adapter/ListAdapter.kt
+++ app/src/main/java/com/example/android_pfti/view/adapter/ListAdapter.kt
@@ -3,7 +3,9 @@ package com.example.android_pfti.view.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
+import androidx.core.widget.doAfterTextChanged
 import androidx.recyclerview.widget.RecyclerView
 import com.example.android_pfti.model.repo.LocationList
 import com.example.android_pfti.R
@@ -18,15 +20,22 @@ class ListAdapter(private var list: LocationList) :
     }
 
     override fun onBindViewHolder(holder: ListAdapterVH, position: Int) {
-        holder.latlngTextView?.text = LocationList.arrayOfMarkers[position].first.toString()
-        holder.nameTextView?.text = LocationList.arrayOfMarkers[position].second.toString()
+        holder.latlngTextView?.text = list.arrayOfMarkers[position].first.toString()
+        holder.nameTextView?.setText(list.arrayOfMarkers[position].second)
+        holder.nameTextView?.doAfterTextChanged { text ->
+            if (!text.isNullOrBlank()) {
+                list.arrayOfMarkers[position] =
+                    Pair(list.arrayOfMarkers[position].first, text.toString())
+            }
+        }
     }
 
     override fun getItemCount() = LocationList.arrayOfMarkers.size
 
     inner class ListAdapterVH(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var latlngTextView: TextView? = null
-        var nameTextView: TextView? = null
+        var nameTextView: EditText? = null
+
         init {
             latlngTextView = itemView.findViewById(R.id.latlng_rv)
             nameTextView = itemView.findViewById(R.id.name_rv)
